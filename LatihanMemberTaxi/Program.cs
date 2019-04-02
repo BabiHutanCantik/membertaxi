09387853f151d3db921bf0acadb64a8baf173a6b
BabiHutanCantikcd <tegarrresa3@gmail.com>
2019-04-02 14:03:22 +0700

    wkwk

---
+++ LatihanMemberTaxi/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMemberTaxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Taxi taxi = new Taxi();
+
+            taxi.DriverName = "Jono";
+            taxi.OnDuty = true;
+            taxi.Numpassanger = 10;
+
+            taxi.TaxiInfo();
+            taxi.PickUpPassanger();
+            taxi.DropOffPassanger();
+            Console.ReadKey();
+        }
+    }
+}
